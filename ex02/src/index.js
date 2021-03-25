65f57cec2f9c6097592e585f62e8fbdb4ea14ed3
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-25 22:50:18 +0100

    added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,46 @@
+function* myGenerator() {
+    yield* insideGenerator1();
+    yield* insideGenerator2();
+    yield* insideGenerator3();
+
+}
+
+function* insideGenerator1() {
+    for (let i = 1; i <= 5; i++) {
+        var x = yield i;
+
+    } 
+}
+
+function* insideGenerator2() {
+    for (let i = 10; i <= 15; i++) {
+        x = yield i;
+    
+    }
+}
+
+function* insideGenerator3() {
+    for (let i = 6; i <= 9; i++) {
+        x = yield i;
+        
+    }
+}
+
+var iterator = myGenerator();
+var fifteenArray = [];
+
+
+for (i = 0; i < 16; i++) {
+    if (i < 15) {
+        fifteenArray.push(iterator.next().value + "#");
+    } else {
+        fifteenArray.push(iterator.next().value + "!");
+
+    }
+}
+
+
+
+
+console.log(fifteenArray);
+module.exports = { fifteenArray, myGenerator};
